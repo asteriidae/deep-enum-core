7331b18513d2fc1771073145550969156cd57dbf
Jacques Germishuys <jacques.germishuys@asterias.io>
2024-01-08 10:25:38 +0000

    chore: make scripts compatible with commonjs

--- scripts/package-size.ts
+++ scripts/package-size.ts
@@ -1,6 +1,10 @@
 import {statSync} from 'fs';
-import {globby} from 'globby';
 
-const paths = await globby(['./dist/*.js']);
-const totalSize = paths.reduce((total, path) => statSync(path).size + total, 0);
-console.log(`${(totalSize * 0.001).toFixed(2)} Kb`);
+async function main() {
+  const {globby} = await import('globby');
+  const paths = await globby(['./dist/*.js']);
+  const totalSize = paths.reduce((total, path) => statSync(path).size + total, 0);
+  console.log(`${(totalSize * 0.001).toFixed(2)} Kb`);
+}
+
+main();
